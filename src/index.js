e3bfa325cb98b487960031651a012e128d02ae27
AlexeyGrk <alexgmirko@gmail.com>
2021-05-04 12:39:59 +0300

    add task 1

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
 import './sass/main.scss';
+import './task-1.js';
 
-const myPromise = new Promise((resolve, reject) => {
-  const result = Math.random() > 0.5;
-  setTimeout(() => {
-    if (result) {
-      resolve('This is success resolve');
-    }
-    reject('This is failed promise');
-  }, 2000);
-});
-myPromise.then(
-  result => {
-    console.log(result);
-  },
-  error => {
-    console.log(error);
-  },
-);
+// const myPromise = new Promise((resolve, reject) => {
+//   const result = Math.random() > 0.5;
+//   setTimeout(() => {
+//     if (result) {
+//       resolve('This is success resolve');
+//     }
+//     reject('This is failed promise');
+//   }, 2000);
+// });
+// myPromise
+//   .then(result => {
+//     console.log(result);
+//   })
+//   .catch(error => console.log(error));
